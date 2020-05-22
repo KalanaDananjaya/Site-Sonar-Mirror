dce2f6e71b30f82c70f8b8e41dfbe96acb3d3c08
KalanaDananjaya <kalana.16@cse.mrt.ac.lk>
2020-05-22 11:24:51 +0530

    Automate the copying of executable to the Grid

--- staging-grid.sh
+++ staging-grid.sh
@@ -4,9 +4,9 @@ set -e
 GRID_HOME="$1"
 BASE_DIR="$GRID_HOME/site-sonar"
 JDL_DIR="$BASE_DIR/JDL"
+SCRIPT_DIR="$BASE_DIR/scripts"
 LOCAL_JDL="JDL"
-
-echo $JDL_DIR
+LOCAL_SCRIPT="scripts"
 
 function stage_JDLs() {
 	local j
@@ -14,13 +14,27 @@ function stage_JDLs() {
 
 	for j in $(ls $LOCAL_JDL)
 	do
-	  cmd="alien_cp file://$LOCAL_JDL/$j alien://$JDL_DIR/$(basename $j)@disk:1"
+	  cmd="alien_cp file:$LOCAL_JDL/$j alien:$JDL_DIR/$(basename $j)@disk:1"
 	  echo $cmd
 	  $cmd &
 	done
 }
 
-#alien.py rm -r -f $BASE_DIR
+function stage_scripts() {
+	local j
+	local cmd
+
+	for j in $(ls $LOCAL_SCRIPT)
+	do
+	  cmd="alien_cp file:$LOCAL_SCRIPT/$j alien:$SCRIPT_DIR/$(basename $j)@disk:1"
+	  echo $cmd
+	  $cmd &
+	done
+}
+
+alien.py rm -r -f $BASE_DIR
 alien.py mkdir -p $JDL_DIR
+alien.py mkdir -p $SCRIPT_DIR
 
 stage_JDLs
+stage_scripts
