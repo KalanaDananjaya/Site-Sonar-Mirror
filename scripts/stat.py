d7e0d08d41e78d6e6dfee111db697f5c4121f6a5
KalanaDananjaya <kalana.16@cse.mrt.ac.lk>
2020-07-08 17:48:37 +0530

    Replace bash job with a MonAlisa notification job

---
+++ scripts/stat.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import os
+from subprocess import Popen,PIPE
+import time  
+
+url = os.environ.get('APMON_CONFIG')
+jobId = os.environ.get('ALIEN_PROC_ID')
+
+with Popen('unzip apmon.zip', stdout=PIPE, bufsize=1, universal_newlines=True, shell=True) as p:
+    out = p.stdout
+
+import apmon
+
+monitors = {
+    'Uname': 'echo $(uname -a)',
+    'HMD': 'echo $HOME',
+    'TMP': 'echo $TMPDIR',
+    'Singularity': 'echo $(singularity exec -B /cvmfs:/cvmfs /cvmfs/alice.cern.ch/containers/fs/singularity/centos7 /bin/echo SUPPORTED)',
+    'Loop devices': 'echo $(cat /etc/singularity/singularity.conf | grep "loop devices")',
+    'Overlay': 'echo $(cat /etc/singularity/singularity.conf | grep "enable overlay" | head -1)',
+    'Underlay': 'echo $(cat /etc/singularity/singularity.conf | grep "enable underlay")',
+    'Container Enabled?': 'echo $(ls / | grep workdir])',
+    'Max namespaces': 'echo $(cat /proc/sys/user/max_user_namespaces)'
+}
+
+
+# Initialize ApMon specifying that it should not send information about the system.
+# Note that in this case the background monitoring process isn't stopped, in case you may
+# want later to monitor a job.
+apm = apmon.ApMon((url,))
+apm.setLogLevel("NOTICE")
+apm.confCheck = False
+apm.enableBgMonitoring(False)
+apm.setMaxMsgRate(75)
+apm.setMaxMsgSize(500)
+
+if not apm.initializedOK():
+    print("It seems that ApMon cannot read its configuration. Setting the default destination")
+else:
+    print("ApMon initialized succesfully")
+
+# you can put as many pairs of parameter_name, parameter_value as you want
+# but be careful not to create packets longer than 8K.
+for key in monitors.keys():
+    command = monitors[key]
+    value = ''
+    with Popen(command, stdout=PIPE, bufsize=1, universal_newlines=True, shell=True) as p:
+        print (command)
+        for line in p.stdout:
+            value += line
+        print (value)
+    apm.sendParameters("SiteSonar", jobId , {str(key): str(value)})
+    time.sleep(.005)
+
+print("Job Completed")
